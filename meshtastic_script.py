92e22513f4a992601eae41a7802257dfcec9c2c2
RamblingBanjo <ramblingbanjo@gmail.com>
2025-04-01 22:51:24 +0100

    pulls data from sensor now

--- meshtastic_script.py
+++ meshtastic_script.py
@@ -1,6 +1,16 @@
 import time
 import meshtastic
 import subprocess
+import board
+from adafruit_ms8607 import MS8607
+
+
+textMessage = "text message not yet defined"
+destNode = '!fcc1b36f'
+
+i2c = board.I2C()
+
+sensor = MS8607(i2c)
 
 def runCmd(cmd):
     process = subprocess.Popen(
@@ -19,16 +29,18 @@ def runCmd(cmd):
 def main():
     
     runCmd("--port")
-    runCmd("--sendtext 'range test' --dest '!fcc1b36f'")
+    runCmd("--sendtext " + textMessage + " --dest " + destNode)
                 
     i = 0
     
-    while i<5:
+    while i<20:
         
         i += 1
         
         try:
-            print("loop is running baby")
+            print(f"Temperature: {sensor.temperature:.2f} C")
+            print(f"Humidity: {sensor.relative_humidity:.2f} %")
+            print(f"Pressure: {sensor.pressure:.2f} hPa")
                 
             time.sleep(1)
 
