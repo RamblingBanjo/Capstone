5284e04373a5869c92f8181f1d3911c41aa5e30d
RamblingBanjo <ramblingbanjo@gmail.com>
2025-04-08 16:04:09 -0500

    added gps data collection and transmission

--- meshtastic_script.py
+++ meshtastic_script.py
@@ -2,7 +2,9 @@ import time
 import meshtastic
 import subprocess
 import board
+import melopero_samm8q as mp
 from adafruit_ms8607 import MS8607
+
 
 textMessage = "text message not yet defined"
 destNode = '!fcc1b36f'
@@ -11,6 +13,9 @@ i2c = board.I2C()
 
 sensor = MS8607(i2c)
 
+gps = mp.SAM_M8Q()
+gps.set_measurement_frequency(5000)
+    
 cmdTimeout = 20
 
 def runCmd(cmd):
@@ -53,7 +58,46 @@ def main():
         i += 1
         
         try:
-            textMessage = (f"{sensor.temperature:.2f}"+f"{sensor.relative_humidity:.2f}"+f"{sensor.pressure:.1f}"+time.strftime("%H%M%S")).replace(".", "")
+            gps.get_pvt()
+            j=0
+            nav_data = gps.pvt_data
+            
+            while j==0:
+                print("waiting for GPS connection")
+                time.sleep(5)
+                gps.get_pvt()
+                nav_data = gps.pvt_data
+                if nav_data:
+                    j = 1
+            
+            
+            print(nav_data)
+            
+            if nav_data:
+                
+                latraw = nav_data["latitude"]
+                print("latraw: " + str(latraw))
+                lat_sign = 0 if latraw > 0 else 1
+                print("lat_sign: " +  str(lat_sign))
+                lat_value = int(abs(latraw) * 100000)
+                print("lat_value: " + str(lat_value))
+                lat_str = f"{lat_sign}{lat_value:08d}"
+                print("lat_str" + lat_str)
+                
+                lonraw = nav_data["longitude"]
+                print("lonraw" + str(lonraw))
+                lon_sign = 0 if lonraw > 0 else 1
+                print("lon_sign" + str(lon_sign))
+                lon_value = int(abs(lonraw) * 100000)
+                print("lon_value" + str(lon_value))
+                lon_str = f"{lon_sign}{lon_value:08d}"
+                print("lon_str" + lon_str)
+                
+                altraw = nav_data["MSL_height"]
+                alt_str = f"{altraw:08d}"
+                print(alt_str)
+                
+            textMessage = (f"{sensor.temperature:.2f}"+f"{sensor.relative_humidity:.2f}"+f"{sensor.pressure:.1f}"+lat_str + lon_str + alt_str + time.strftime("%H%M%S")).replace(".", "")
             
             runCmd("--sendtext '" + textMessage + "' --dest " + destNode)
 
